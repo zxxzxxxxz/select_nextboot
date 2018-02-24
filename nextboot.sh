72472c46826d416a424f5a52735573d68cfff096
zxxzxxxxz <zxxzxxxxz@gmail.com>
2018-02-24 17:26:03 +0900

    Update nextboot.sh

--- nextboot.sh
+++ nextboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly BOOT_NAME="ubuntu"
+readonly BOOT_NAME="ubuntu"  # uefi entry name
 
 /bin/efibootmgr | grep -E '^Boot[0-9]{4}(\*| ) .*'$BOOT_NAME'.*' | sed -E 's/^Boot//g' | sed -E 's/(\*| ) .*'$BOOT_NAME'.*//g' | while read num;
 do
