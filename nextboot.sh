89bc6c71cf0db9d8d34d16fcb1ed70882e49d848
poini_coon <zxxzxxxxz@gmail.com>
2018-02-24 17:21:34 +0900

    first commit

---
+++ nextboot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+readonly BOOT_NAME="ubuntu"
+
+/bin/efibootmgr | grep -E '^Boot[0-9]{4}(\*| ) .*'$BOOT_NAME'.*' | sed -E 's/^Boot//g' | sed -E 's/(\*| ) .*'$BOOT_NAME'.*//g' | while read num;
+do
+  efibootmgr --bootnext $num && reboot
+done
